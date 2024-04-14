136d7b51af5db3ad97e38916d1b6b0d0a2deddea
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-04-14 14:51:20 -0400

    Quiet wget

--- scripts/cibw_linux_before_all.sh
+++ scripts/cibw_linux_before_all.sh
@@ -36,26 +36,26 @@ echo $BUILD_ARCH
 
 case "$BUILD_ARCH" in
     x86_64)
-        wget https://go.dev/dl/go1.20.12.linux-amd64.tar.gz
+        wget -q https://go.dev/dl/go1.20.12.linux-amd64.tar.gz
         tar -C /usr/local -xzf go1.20.12.linux-amd64.tar.gz
         ;;
     i686)
-        wget https://go.dev/dl/go1.20.12.linux-386.tar.gz
+        wget -q https://go.dev/dl/go1.20.12.linux-386.tar.gz
         tar -C /usr/local -xzf go1.20.12.linux-386.tar.gz
         ;;
     
     ppc64le)
-        wget https://go.dev/dl/go1.20.12.linux-ppc64le.tar.gz
+        wget -q https://go.dev/dl/go1.20.12.linux-ppc64le.tar.gz
         tar -C /usr/local -xzf go1.20.12.linux-ppc64le.tar.gz
         ;;
     
     aarch64)
-        wget https://go.dev/dl/go1.20.12.linux-arm64.tar.gz
+        wget -q https://go.dev/dl/go1.20.12.linux-arm64.tar.gz
         tar -C /usr/local -xzf go1.20.12.linux-arm64.tar.gz
         ;;
 
     s390x)
-        wget https://go.dev/dl/go1.20.12.linux-s390x.tar.gz
+        wget -q https://go.dev/dl/go1.20.12.linux-s390x.tar.gz
         tar -C /usr/local -xzf go1.20.12.linux-s390x.tar.gz
         ;;
 
