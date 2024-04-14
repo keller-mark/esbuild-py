642d6962d716b59e4b49aec5fb9ef4e2faaae45b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-04-14 11:07:09 -0400

    Just use list of paths to try

--- src/esbuild_py/esbuild.py
+++ src/esbuild_py/esbuild.py
@@ -3,23 +3,26 @@ from distutils.sysconfig import get_config_var
 from pathlib import Path
 import os
 import site
-import sys
 
 # Get path to shared library (.so file)
 # References:
 # - https://github.com/ardanlabs/python-go/blob/db361abf/pyext/checksig.py#L7
 # - https://github.com/jopohl/urh/blob/8886970/src/urh/util/util.py#L93
 # - https://github.com/dmlc/xgboost/blob/882f413/python-package/xgboost/libpath.py#L14
+# - https://stackoverflow.com/a/77824551
 def get_lib_path():
     ext_suffix = get_config_var('EXT_SUFFIX')
     so_filename = ('_esbuild' + ext_suffix)
-    if getattr(sys, "frozen", False):
-        # Not installed as editable
-        so_dir = site.getsitepackages()[0]
-    else:
-        # Installed as editable
-        so_dir = str(Path(__file__).absolute().parent.parent)
-    return os.path.join(so_dir, so_filename)
+
+    dirs_to_try = [
+        # For running in editable mode
+        str(Path(__file__).absolute().parent.parent),
+        site.getsitepackages()[0]
+    ]
+    for d in dirs_to_try:
+        so_path = os.path.join(d, so_filename)
+        if os.path.exists(so_path):
+            return so_path
 
 # Load functions from shared library and set their signatures
 # References:
