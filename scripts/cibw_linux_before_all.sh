5ce4b46e98580b985cef6e1c6976bc41079bbe9d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-04-14 14:05:01 -0400

    Use script

---
+++ scripts/cibw_linux_before_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Configure the libraries needed to build wheel packages on linux.
+# This script is designed to be used by cibuildwheel as CIBW_BEFORE_ALL_LINUX
+
+set -euo pipefail
+set -x
+
+case "$ID" in
+    debian)
+        apt-get update
+        apt-get -y upgrade
+        apt-get -y install libpq-dev
+        ;;
+
+    centos)
+        yum install wget -y
+        ;;
+
+    *)
+        echo "$0: unexpected Linux distribution: '$ID'" >&2
+        exit 1
+        ;;
+esac
+
+wget https://go.dev/dl/go1.20.12.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.12.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go get github.com/keller-mark/esbuild-py
